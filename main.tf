0dd80d084d09e5216ac3fbcec6d905acc3443704
Fekri Saleh <fekri600@gmail.com>
2025-07-05 21:23:38 -0600

    resource group

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ module "network" {
   environment      = terraform.workspace
   project_settings = var.project_settings
   network          = var.network[terraform.workspace]
-  resource_group_name = trim(file("bootstrap/outputs/.backend_rg"))
-}
+  resource_group_name = trimspace(file("bootstrap/outputs/.backend_rg"))
+  }
