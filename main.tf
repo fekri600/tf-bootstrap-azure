17f857caf22403bb20f6015653c36d2042985725
Fekri Saleh <fekri600@gmail.com>
2025-07-05 21:20:15 -0600

    fix the resource group path

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ module "network" {
   environment      = terraform.workspace
   project_settings = var.project_settings
   network          = var.network[terraform.workspace]
-  resource_group_name = trim(file("${loca.bootstrap_output/rg.txt}"))
+  resource_group_name = trim(file("bootstrap/outputs/.backend_rg"))
 }
